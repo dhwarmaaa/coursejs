913c7060445093b5806b41d8eff4a452c94b97b9
Dharma Saucedo <81653890+dhwarmaaa@users.noreply.github.com>
2023-01-24 20:01:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-const mybody = document.getElementsByTagName('body')[0];
-/// login
+// login form
 const inputLoginUsername = document.getElementById('user');
+const loginForm = document.getElementById('loginForm');
 const inputLoginPin = document.getElementById('pswd');
-const submit = document.getElementById('submit');
-const prueba = document.getElementById('prueba');
-const salida = document.getElementById('salida');
+const submitLogin = document.getElementById('submit');
+const errorMessage = document.getElementById('errorMessage');
+
+// exit
+const logout = document.getElementById('exit');
+
+// body app
 const nav = document.getElementById('navBar');
 const bodyApp = document.getElementById('main');
-const modalLogin = document.getElementById('login');
-const exit = document.getElementById('exit');
-const over = document.getElementById('overlay');
+const bodyContent = document.getElementById('mainContent');
+const footer = document.getElementById('footer');
+
 // buttons
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
-// select
+const buttonBack = document.getElementById('back');
+
+// search
 const select_month = document.getElementById('selectMonth');
 const select_year = document.getElementById('selectYear');
 const button_search = document.getElementById('buttonSearch');
-// pagination
-const pagination = document.getElementById('pagination');
 
-// select values
-// prettier ignore
+const state = {
+  page: 1,
+  resultsPerPage: 3,
+  totalPages: 0,
+};
+
+// select month
 const month = [
   'Enero',
   'Febrero',
@@ -38,64 +47,105 @@ const month = [
 ];
 
 const year = Array.from({ length: 80 }, (cur, i) => 2000 + i);
-
-const state = {
-  page: 1,
-  resultsPerPage: 3,
-  totalPages: 0,
-};
-
+///////////////////////////////////////////////////////
 // lectura del JSON
-const response = await fetch('data.json');
-const response_format = await response.json();
-const data = JSON.stringify(response_format);
-const users = JSON.parse(data);
+let users;
+(async function () {
+  const response = await fetch('./data.json');
+  const response_format = await response.json();
+  const data = JSON.stringify(response_format);
+  users = JSON.parse(data);
+})();
 
-const getSearchResultsPage = function (page = state.page, data) {
-  state.page = page;
+//////////////////////////////////////////////////
 
-  const start = (page - 1) * state.resultsPerPage; // 0
-  const end = page * state.resultsPerPage; // 9
-  console.log(data.slice(start, end));
-  dataTable(currentAccount.payslip.slice(start, end));
-  // return state.search.results.slice(start, end);
+const fillSelect = function (selectId, data) {
+  const select = document.querySelector(selectId);
+  data.forEach(element => {
+    const option = document.createElement('option');
+    option.text = element;
+    option.value = element;
+    select.appendChild(option);
+  });
 };
 
+////////////////////////////////////////////////////
+
+const fillData = function (id, data) {
+  const component = document.getElementById(id);
+  component.innerHTML = '';
+  component.innerHTML = data;
+};
+
+///////////////////////////////////////////////////
+
+const old = dateStart => {
+  const [dayStart, monthStart, yearStart] = dateStart.split('/');
+  const dateStartFormat = new Date(yearStart, monthStart - 1, dayStart);
+  const today = new Date();
+  const difference = Math.abs(today - dateStartFormat);
+  const yearsTotal = (difference / (1000 * 3600 * 24) / 365).toFixed(1);
+  return yearsTotal;
+};
+
+const fillInfo = currentAccount => {
+  // welcome message
+  fillData('welcomeMessage', `Bienvenido ${currentAccount.name}`);
+  // data
+  fillData('code', `${currentAccount.id}`);
+  fillData('name', `${currentAccount.name} ${currentAccount.lastname}`);
+  fillData('birthday', `${currentAccount.birthday}`);
+  fillData('startDate', `${currentAccount.startDate}`);
+  fillData('shift', `${currentAccount.shift}`);
+  fillData(
+    'vacations',
+    `${currentAccount.vacationsTotal - currentAccount.vacationsUse}`
+  );
+  fillData('position', `${currentAccount.position}`);
+  fillData('old', `${old(currentAccount.startDate)} años`);
+};
+
+/////////////////////////////////////////////////////
+
+const numPagesPerPage = dataSize => {
+  const numPages =
+    dataSize?.length === undefined
+      ? 0
+      : Math.ceil(dataSize.length / state.resultsPerPage);
+  return numPages;
+};
+
+////////////////////////////////////////////////////
+
+// table
 const dataTable = function (data) {
-  console.log(data?.length);
-  // para la primera pagina
+  // first page
   if (state.page <= 0 || state.page === 1) {
     previous.style.visibility = 'hidden';
   }
-  // para la ultima
+  // last page
   if (state.page === state.totalPages) {
     next.style.visibility = 'hidden';
   }
 
-  // creates <table> and <tbody> elements
-  const mytable = document.querySelector('u-table-entity');
   const mytablebody = document.getElementById('tbody');
-
   // creating all cells
   const body = data
     ?.map((item, i) => {
-      return `<tr style="height: 45px" onClick=${console.log(
-        'funciono: ' + i
-      )}> 
-    <td class="u-table-cell">${item.month}</td>
-    <td class="u-table-cell">${item.payday}</td>
-    <td class="u-table-cell">${item.periodEnd}</td>
-    <td class="u-table-cell">${item.concept}</td>
-    <td class="u-table-cell">$ ${
-      item.total
-    } <button class="botonModal">Ver detalle</button></td>
+      return `<tr> 
+    <td>${item.month}</td>
+    <td>${item.payday}</td>
+    <td>${item.periodEnd}</td>
+    <td>${item.concept}</td>
+    <td>$ ${item.total}</td>
+    <td><button type="button" class="btn btn-link btn-sm btn-rounded botonModal" id=${item.id}>Ver detalle</button></td>
     </tr>`;
     })
     .join('');
   mytablebody.innerHTML = body;
 };
 
-/* para el boton de ver a detalle*/
+// button Ver detalle
 const on = (element, event, selector, handler) => {
   element.addEventListener(event, e => {
     // target implementa una delegacion del evento
@@ -105,34 +155,43 @@ const on = (element, event, selector, handler) => {
   });
 };
 
+/// Deducciones
+const ISR = sueldo => {
+  const isr = (sueldo * 2.267) / 100;
+  return isr.toFixed(2);
+};
+
+const IMSS = sueldo => {
+  const imss = (sueldo * 1.134) / 100;
+  return imss.toFixed(2);
+};
+
+const INFONAVIT = sueldo => {
+  const infonavit = (sueldo * 1.638) / 100;
+  return infonavit.toFixed(2);
+};
+
+const extra = hours => {
+  return (hours * 60).toFixed(2);
+};
+
 on(document, 'click', '.botonModal', e => {
   const fila = e.target.parentNode;
-  const id = fila.firstElementChild;
-  console.log(id);
+  const id = +fila.firstElementChild.getAttribute('id') - 1;
+
   const modal = document.querySelector('.modalData');
-  const overlay = document.querySelector('.overlay');
+  //const overlay = document.querySelector('.overlay');
   const btnCloseModal = document.querySelector('.close-modal');
   /* LLENADO DE LA NOMINA DEL MODAL */
-  /// Deducciones
-  const ISR = sueldo => {
-    const isr = (sueldo * 2.267) / 100;
-    return isr.toFixed(2);
-  };
 
-  const IMSS = sueldo => {
-    const imss = (sueldo * 1.134) / 100;
-    return imss.toFixed(2);
-  };
-
-  const INFONAVIT = sueldo => {
-    const infonavit = (sueldo * 1.638) / 100;
-    return infonavit.toFixed(2);
-  };
   const totalDeducciones = document.getElementById('total_deducciones');
+  const totalPercepciones = document.getElementById('total_percepciones');
+  const neto = document.getElementById('neto');
+
   const deducciones = [
-    +ISR(currentAccount.payslip[0].base),
-    +IMSS(currentAccount.payslip[0].base),
-    +INFONAVIT(currentAccount.payslip[0].base),
+    +ISR(currentAccount.payslip[id].base),
+    +IMSS(currentAccount.payslip[id].base),
+    +INFONAVIT(currentAccount.payslip[id].base),
   ];
 
   totalDeducciones.innerHTML = `$ ${deducciones.reduce((a, b) => a + b)}`;
@@ -150,9 +209,6 @@ on(document, 'click', '.botonModal', e => {
   const isr = document.getElementById('ISR');
   const infonavit = document.getElementById('INFONAVIT');
 
-  const extra = hours => {
-    return (hours * 60).toFixed(2);
-  };
   nameNomina.innerHTML = '';
   RFC.innerHTML = '';
   CURP.innerHTML = '';
@@ -171,22 +227,36 @@ on(document, 'click', '.botonModal', e => {
   nameNomina.innerHTML = `${currentAccount.name} ${currentAccount.lastname}`;
   RFC.innerHTML = currentAccount.rfc;
   CURP.innerHTML = currentAccount.curp;
-  period.innerHTML = `${currentAccount.payslip[0].periodStart} al ${currentAccount.payslip[0].periodEnd}`;
+  period.innerHTML = `${currentAccount.payslip[id].periodStart} al ${currentAccount.payslip[id].periodEnd}`;
   // percepciones
-  base.innerHTML = `$ ${currentAccount.payslip[0].base.toFixed(2)}`;
-  holiday.innerHTML = `$ ${currentAccount.payslip[0].holiday.toFixed(2)}`;
-  overtime.innerHTML = `$ ${extra(currentAccount.payslip[0].overtime)}`;
-  despensa.innerHTML = `$ ${currentAccount.payslip[0].despensa.toFixed(2)}`;
-  punctuality.innerHTML = `$ ${currentAccount.payslip[0].punctuality.toFixed(
+
+  base.innerHTML = `$ ${currentAccount.payslip[id].base.toFixed(2)}`;
+  holiday.innerHTML = `$ ${currentAccount.payslip[id].holiday.toFixed(2)}`;
+  overtime.innerHTML = `$ ${extra(currentAccount.payslip[id].overtime)}`;
+  despensa.innerHTML = `$ ${currentAccount.payslip[id].despensa.toFixed(2)}`;
+  punctuality.innerHTML = `$ ${currentAccount.payslip[id].punctuality.toFixed(
     2
   )}`;
-  attendance.innerHTML = `$ ${currentAccount.payslip[0].attendance.toFixed(2)}`;
+  attendance.innerHTML = `$ ${currentAccount.payslip[id].attendance.toFixed(
+    2
+  )}`;
+  const sum_percepciones =
+    +currentAccount.payslip[id].base.toFixed(2) +
+    +currentAccount.payslip[id].holiday.toFixed(2) +
+    +extra(currentAccount.payslip[id].overtime) +
+    +currentAccount.payslip[id].despensa.toFixed(2) +
+    +currentAccount.payslip[id].punctuality.toFixed(2) +
+    +currentAccount.payslip[id].attendance.toFixed(2);
   // deducciones
   imss.innerHTML = `$ ${deducciones[1]}`;
   isr.innerHTML = `$ ${deducciones[0]}`;
   infonavit.innerHTML = `$ ${deducciones[2]}`;
   // totales
+  totalPercepciones.innerHTML = `$ ${sum_percepciones.toFixed(2)}`;
 
+  neto.innerHTML = `$ ${(
+    +sum_percepciones - +deducciones.reduce((a, b) => a + b)
+  ).toFixed(2)}`;
   modal.classList.remove('hidden');
   // overlay.classList.remove('hidden');
   const closeModal = function () {
@@ -195,30 +265,19 @@ on(document, 'click', '.botonModal', e => {
   };
 
   btnCloseModal.addEventListener('click', closeModal);
-  //overlay.addEventListener('click', closeModal);
-  // openModal();
 });
 
-const fillSelect = function (selectId, data) {
-  const select = document.querySelector(selectId);
-  data.forEach(element => {
-    const option = document.createElement('option');
-    option.text = element;
-    option.value = element;
-    select.appendChild(option);
-  });
+const getSearchResultsPage = function (page = state.page, data) {
+  state.page = page;
+  const start = (page - 1) * state.resultsPerPage; // 0
+  const end = page * state.resultsPerPage; // 9
+  dataTable(currentAccount.payslip.slice(start, end));
 };
 
-const fillData = function (id, data) {
-  const component = document.getElementById(id);
-  component.innerHTML = '';
-  component.innerHTML = data;
-};
-
+// buttons
 next.addEventListener('click', function () {
   state.page += 1;
   getSearchResultsPage(state.page, currentAccount.payslip);
-
   if (state.page > 1) {
     previous.style.visibility = 'visible';
   }
@@ -236,105 +295,108 @@ previous.addEventListener('click', function () {
   }
 });
 
+buttonBack.addEventListener('click', function () {
+  dataTable(currentAccount.payslip?.slice(0, state.resultsPerPage));
+  buttonBack.classList.add('hidden');
+  previous.classList.remove('hidden');
+  next.classList.remove('hidden');
+});
+///////////////////////////////////////////////////
+
+// search
+
 button_search.addEventListener('click', function () {
   // validar que haya un mes
   const selectedMonth =
-    select_month.value !== 'Item 1' ? select_month.value : 'error';
+    select_month.value !== 'Mes' ? select_month.value : 'error';
 
   // validar que haya un año
   const selectedYear =
-    select_year.value !== 'Item 1' ? select_year.value : 'error';
+    select_year.value !== 'Año' ? select_year.value : 'error';
 
   // mensaje de error
-
+  console.log(`mes: ${selectedMonth} año: ${selectedYear}`);
   // filtrado de datos
   const results = currentAccount.payslip.filter(
     data => data.month === selectedMonth && data.year === selectedYear
   );
-  const resultsMap = results.map(item => {
-    return {
-      month: `${item.month}`,
-      year: `${item.year}`,
-      payday: `${item.payday}`,
-      periodEnd: `${item.periodEnd}`,
-      concept: `${item.concept}`,
-      total: `${item.total}`,
-    };
-  });
+  if (results.length != 0) {
+    const resultsMap = results.map(item => {
+      return {
+        id: `${item.id}`,
+        month: `${item.month}`,
+        year: `${item.year}`,
+        payday: `${item.payday}`,
+        periodStart: `${item.periodStart}`,
+        periodEnd: `${item.periodEnd}`,
+        concept: `${item.concept}`,
+        total: `${item.total}`,
+        base: `${item.base}`,
+        holiday: `${item.holiday}`,
+        overtime: `${item.overtime}`,
+        despensa: `${item.despensa}`,
+        attendance: `${item.attendance}`,
+        punctuality: `${item.punctuality}`,
+      };
+    });
+    console.log(resultsMap);
+    dataTable(resultsMap);
+    buttonBack.classList.remove('hidden');
+  }
 
-  dataTable(resultsMap);
-  alert(`tam: ${results.length} resultado: ${resultsMap} `);
+  if (results.length == 0) {
+    const message = 'No se encontraron resultados:(';
+    const mytablebody = document.getElementById('tbody');
+    mytablebody.innerHTML = message;
+    buttonBack.classList.remove('hidden');
+    previous.classList.add('hidden');
+    next.classList.add('hidden');
+  }
+
+  select_month.value = 'Mes';
+  select_year.value = 'Año';
 });
 
-// inicialización de componentes
+////////////////////////////////////////////////////
 const init = currentAccount => {
-  // select
+  // selects
   fillSelect('#selectMonth', month);
   fillSelect('#selectYear', year);
-
-  // bienvenida
-  fillData('welcomeMessage', `Bienvenido ${currentAccount.name}`);
-  // datos
-  fillData('code', `${currentAccount.id}`);
-  fillData('name', `${currentAccount.name} ${currentAccount.lastname}`);
-  fillData('birthday', `${currentAccount.birthday}`);
-  fillData('startDate', `${currentAccount.startDate}`);
-  fillData('shift', `${currentAccount.shift}`);
-  fillData(
-    'vacations',
-    `${currentAccount.vacationsTotal - currentAccount.vacationsUse}`
-  );
-  fillData('position', `${currentAccount.position}`);
-  const day1 = new Date(currentAccount.startDate);
-  const date = new Date();
-  const day2 = new Date(date.toISOString().split('T')[0]);
-
-  const difference = Math.abs(date - day1);
-  const days = difference / (1000 * 3600 * 24);
-  fillData('old', `${difference} años`); // fecha actual - fecha de ingreso
-
-  /// tabla
-  const numPages =
-    currentAccount.payslip?.length === undefined
-      ? 0
-      : Math.ceil(currentAccount.payslip.length / state.resultsPerPage);
-
-  state.totalPages = numPages;
-
-  // llenado de la tabla
+  // info tab
+  fillInfo(currentAccount);
+  state.totalPages = numPagesPerPage(currentAccount.payslip);
+  // table
   dataTable(currentAccount.payslip?.slice(0, state.resultsPerPage));
-  if (state.page === 1) {
-    previous.style.visibility = 'hidden';
-  }
 };
 
-/////////////////////////////////////////
-// Login
-
+///////////////////////////////////////////////////
+// login
 let currentAccount = '';
-//init();
 
-submit.addEventListener('click', function (e) {
-  // Prevent form from submitting
+submitLogin.addEventListener('click', function (e) {
   e.preventDefault();
 
+  // validate username
   currentAccount = users.find(acc => acc.username === inputLoginUsername.value);
 
   console.log(currentAccount);
   if (currentAccount?.pass === inputLoginPin.value) {
-    // quitar la opacidad al cuerpo y al head
-    nav.style.opacity = 100;
-    bodyApp.style.opacity = 100;
-    prueba.innerHTML = ' ';
+    // remove hidden
+    nav.classList.remove('hidden');
+    bodyContent.classList.remove('hidden');
+    bodyApp.classList.remove('hidden');
+    footer.classList.remove('hidden');
+    // clean message label
+    errorMessage.innerHTML = ' ';
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
-    modalLogin.classList.add('hidden');
-    over.classList.add('hidden');
+
+    loginForm.classList.add('hidden');
     // Update UI
     init(currentAccount);
   } else {
-    prueba.innerHTML =
+    errorMessage.innerHTML =
       'La informacion ingresada no es correcta. Intente de nuevo';
   }
 });
@@ -344,50 +406,15 @@ submit.addEventListener('click', function (e) {
 // exit
 const exitApp = () => {
   currentAccount = '';
-  nav.style.opacity = 0;
-  bodyApp.style.opacity = 0;
-  modalLogin.classList.remove('hidden');
+  // remove hidden
+  nav.classList.add('hidden');
+  bodyContent.classList.add('hidden');
+  bodyApp.classList.add('hidden');
+  footer.classList.add('hidden');
+  // add login form
+  loginForm.classList.remove('hidden');
 };
 
-const logout = document.getElementById('exit');
 logout.addEventListener('click', function () {
-  alertify.confirm(
-    '¿Ya te vas? ¿Estas seguro que deseas salir?',
-    function () {
-      exitApp();
-    },
-    function () {}
-  );
+  exitApp();
 });
-
-/// modal
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
-
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
-/* on asigna eventos a los elementos del dom*/
